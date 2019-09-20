bc6bcc0b6408295a39719584baaa9f27fffa70af
jdhedden@gmail.com <Jerry_Hedden@cable.comcast.com>
2019-09-20 15:55:41 -0400

    v0.0.0

---
+++ cfg/my_cfg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+<<'__NOTES__'
+
+Configuration Customizations
+
+Use is file for setting/modifying an configuration parameters for COSA.
+
+__NOTES__
+
+# For board that displays UNICODE chess pieces
+FANCY_BOARD=true
+# For plain text
+#FANCY_BOARD=false
+
+
+# Stockfish location
+#ENG[ENG]=$HOME/games/stockfish/stockfish
+
+# If you don't have Stockfish
+#unset ENG[ENG]
+
+
+# For 'Open line in new window' command...
+# ... if using X
+#WINDOW_CMD='xterm -e %s'
+#WINDOW_BKG=true
+
+# .. if you're using XFCE, try this:
+#WINDOW_CMD='xfce4-terminal --zoom=2 --geometry=56x58 -x %s'
+#WINDOW_BKG=true
+
+# EOF
--- lib/engine.sh
+++ lib/engine.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-declare -A ENG=(
-    [TO]=$COSA/tmp/stockfish_cmd.fifo
-    [FM]=$COSA/tmp/stockfish_out.fifo
-    [LOG]=$COSA/log/stockfish.log
-
-    [/Threads]=8
-    [/Hash]=8000
-    [/MultiPV]=5
-    [/Contempt]=100
-
-    [depth]=25
-
-    [KILL_TIME]=3
-    [SHOW]=false
-)
-
-
 ce_params() {
     # USAGE: de_params
     local ep_prm=(Depth Lines Threads Hash Contempt)
@@ -88,17 +71,8 @@ ce_run() {
 
     local ii
 
-    # Platform specific configs
-    if [[ $LOC == home ]]; then
-        ENG[ENG]=/usr/games/stockfish
-
-    elif [[ $LOC == phone ]]; then
-        ENG[ENG]=$HOME/bin/stockfish-10-arm65v8
-
-        ENG[/UCI_AnalyseMode]=true
-
-    else
-        rn_res[err]="Engine not available on this platform: LOC='$LOC'"
+    if [[ ! -f ${ENG[ENG]} ]]; then
+        rn_res[err]='Engine not available on this platform'
         return 1
     fi
 
@@ -142,6 +116,8 @@ __PARAMS__
             echo "$REPLY" >>${ENG[LOG]}
             if [[ $REPLY == uciok ]]; then
                 break
+            elif [[ $REPLY =~ UCI_AnalyseMode ]]; then
+                ENG[/UCI_AnalyseMode]=true
             fi
         fi
     done
@@ -230,26 +206,17 @@ ce_engine() {
     # Run engine
     local -A en_res
     local en_fen
-    if [[ $LOC == office ]]; then
-        # Simulate on laptop
-        en_res[time]=15
-
-        # Philidor $ eng
-        en_fen="2kr3r/1pp1qpb1/p2pb1p1/4n2p/4PBP1/2N1QP1P/PPP1B3/1K1R3R w - - 5 17"
-        en_res[1]="info depth 15 seldepth 23 multipv 1 score cp 115 nodes 11508520 nps 8998060 hashfull 5 tbhits 0 time 1279 pv f4g5 f7f6 g5h4 e5c6 a2a3 c8b8 f3f4 h5g4 h3g4 d8e8 h4f2 h8h1 d1h1 f6f5 g4f5 g6f5 h1h7 e6g8 c3d5"
-        en_res[2]="info depth 15 seldepth 20 multipv 2 score cp 110 nodes 11508520 nps 8998060 hashfull 5 tbhits 0 time 1279 pv h1e1 e5c6 f4g5 f7f6 g5f4 h5g4 h3g4 e7f7 c3d5 d8e8 e3a3 f6f5 g4f5 g6f5 f4e3 f5e4 f3e4 h8h3"
-        en_res[3]="info depth 15 seldepth 30 multipv 3 score cp 100 nodes 11508520 nps 8998060 hashfull 5 tbhits 0 time 1279 pv e3d4 e7f6 d4e3 f6e7 g4g5 e5c6 a2a3 c8b8 c3d5 e6d5 d1d5 g7e5 f4e5 d6e5 c2c3 f7f6 g5f6 e7f6"
-        en_res[5]="info depth 15 seldepth 22 multipv 5 score cp 98 nodes 11508520 nps 8998060 hashfull 5 tbhits 0 time 1279 pv h1g1 h5g4 h3g4 c8b8 f4g5 f7f6 g5f4 e5c4 e3f2 d8e8 f4c1 e7d7 f3f4 f6f5 g4f5 g6f5 f2f1 d7f7"
-
-        # Dutch 7b eng
-        #en_fen="rnbq1rk1/pp2p1bp/2pp1np1/5p2/2PP4/2N2NP1/PP2PPBP/R1BQ1RK1 w - - 0 8"
-        #en_res[1]="info depth 18 seldepth 27 multipv 1 score cp 165 nodes 42919235 nps 2880292 hashfull 38 tbhits 0 time 14901 pv d1b3 b8a6 f1d1 c6c5 b3c2 c8d7 a2a3 c5d4 f3d4 a6c5 b2b4 c5e4 g2e4 f5e4 c3e4 e7e5 d4b5 f6e4 c2e4"
-        #en_res[2]="info depth 18 seldepth 24 multipv 2 score cp 164 nodes 42919235 nps 2880292 hashfull 38 tbhits 0 time 14901 pv d1c2 b8a6 f1d1 c8e6 b2b3 e6d7 c1g5 a6c7 e2e3 b7b5 g5f6 g7f6 c3e2 b5c4 b3c4 a8b8 h2h4 c7e6"
-        #en_res[3]="info depth 18 seldepth 29 multipv 3 score cp 160 nodes 42919235 nps 2880292 hashfull 38 tbhits 0 time 14901 pv d1d3 b8a6 a2a3 a6c7 f1d1 c8d7 c1g5 a8b8 e2e3 b7b5 c4b5 c6b5 g5f6 g7f6 d3c2 f6g7 b2b4"
-        #en_res[4]="info depth 18 seldepth 27 multipv 4 score cp 159 nodes 42919235 nps 2880292 hashfull 38 tbhits 0 time 14901 pv b2b3 e7e5 d4e5 d6e5 c1a3 d8d1 a1d1 f8e8 e2e4 a7a5 e4f5 c8f5 f1e1 e5e4 f3d4 b8a6 d4c2 a8d8 c2e3 d8d3 a3b2 a6c5"
-        #en_res[5]="info depth 18 seldepth 27 multipv 5 score cp 148 nodes 42919235 nps 2880292 hashfull 38 tbhits 0 time 14901 pv c1e3 b8d7 d4d5 c6d5 c4d5 d7c5 a1c1 d8a5 a2a3 c5e4 f3g5 c8d7 g5e4 f6e4 e3d4 g7d4 d1d4 e4c3 c1c3 a5b6"
-
+    if [[ -v "SIMULATED[fen]" ]]; then
+        # Simulated engine results
+        en_fen=${SIMULATED[fen]}
+        en_res[time]=${SIMULATED[time]}
+        local en_cnt=1
+        while [[ -n ${SIMULATED[$en_cnt]} ]]; do
+            en_res[$en_cnt]=${SIMULATED[$en_cnt]}
+            (( en_cnt++ ))
+        done
     else
+        # Run chess engine
         node_get $1 $2.$3.$4.f en_fen
         if ! ce_run "$en_fen" en_res; then
             GBL[ERR]=${en_res[err]}
@@ -317,17 +284,12 @@ ce_engine() {
     local en_file
     part -$ . "${GBL[DB_FILE]}" en_file
     part +$ / "$en_file" en_file
-    en_file="${UTIL[DAT]}/engine/${en_file}_$2-$3-$4.dat"
+    en_file="$COSA/dat/engine/${en_file}_$2-$3-$4.dat"
     cd_save en_db "$en_file"
 
     # Launch results in new window
     cv_window "$en_file" $en_l.$en_tt.$en_ss
     return 0
 }
-
-
-<<'__NOTES__'
-
-__NOTES__
 
 # EOF
